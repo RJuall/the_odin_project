cee50996a7edff10b4c3a0c6aa45ec62179c1e62
RJuall <RJuall@gmail.com>
2015-02-27 01:15:15 -0500

    Implemented user controls
    After lots of headaches....

--- sketch/assets/script/sketchScript.js
+++ sketch/assets/script/sketchScript.js
@@ -2,31 +2,51 @@
 
 $(document).ready(function () {
  
- 
- 
- function createTable(id, num) {
-   var quo = num;
+ function changeColor(){
+  $('#tab').find('td').on('mouseover', function() {
+   $(this).css('background-color', 'aqua');
+  });
+ }
+  
+ function newGrid(num) {
+   var $tableArea = $('#tab');
   
    if (num <= 0) {
+    console.log('error');
     return;
    } else if (num.isNaN) {
+    console.log('error');
     return;
-   } else if (num >= 100) {
+   } else if (num > 100) {
+    console.log('error');
     return;
    }
   
+   $tableArea.empty();
+  
    for (i = 0; i < num; i++) {
-    id.append("<tr></tr>");
+    $tableArea.append("<tr></tr>");
    }
   
    for (i = 0; i < num; i++) {
     for (y = 0; y < num; y++) {
-     id.find('tr').eq(i).append("<td></td>");
+     $tableArea.find('tr').eq(i).append("<td></td>");
     }
    }
-  
+   
+   changeColor();  
  }
  
- createTable($('#tab'), 20);
-  
+ $('#reset').on('click', function() {
+   $('#tab').find('td').css('background-color', 'black');
+ });
+ 
+ $('#changeSize').submit(function() {
+   var newGridval = $('#gsize').val();
+   newGrid(newGridval);
+   event.preventDefault();
+ });
+ 
+ changeColor();
+   
 });
